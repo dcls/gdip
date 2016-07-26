68f9fb2f27a1bae14c4c5d87ee3e49fa92fd7092
Stephen Turner <vustephen@gmail.com>
2016-07-26 17:07:09 +0000

    srst2 multiple samples

--- analysis/pulsenet13.sh
+++ analysis/pulsenet13.sh
@@ -45,13 +45,12 @@ mv SRR3669929_1.fastq.gz PNUSAS002486_1.fastq.gz ; mv SRR3669929_2.fastq.gz PNUS
 
 ## get the scripts you'll need to prepare directories for running cfsan
 git clone https://github.com/dcls/gdip
-cat ~/gdip/scripts/cfsan-prepare-directories.sh
+~/gdip/scripts/cfsan-prepare-directories.sh -h
 
 ## Set up directory structure
 cd ~/exampledata
 ~/gdip/scripts/cfsan-prepare-directories.sh
 cd cfsan
-tree
 
 ## Run the pipeline. (in a screen session: `screen -S cfsan`, and ctrl-A, D to detach)
 # On an m2.2xlarge node (8 cores), this takes ~1hr walltime.
@@ -118,13 +117,77 @@ FastTree -quiet -nt -gtr snpma.fasta \
   | ete view -t /dev/stdin --ss --sbl -i png --Iw 1800 --Ih 1200
 ## Rerooted rendered tree png file is saved in t0.png
 
+
 ################################################################################
-## Run SRST2 on a few samples
+## Run SRST2 for antibiotic resistance for a single sample
 ################################################################################
 
 cd ~/exampledata
 
-# MLST search
+time srst2 --log \
+  --input_pe PNUSAS002249_1.fastq.gz PNUSAS002249_2.fastq.gz \
+  --output   srst2/PNUSAS002249-amr \
+  --gene_db  /home/ubuntu/srst2/data/ARGannot.r1.fasta
+
+# Takes ~3 minutes
+# Results in two files:
+
+# full results file: srst2/PNUSAS002249-amr__fullgenes__ARGannot.r1__results.txt
+# ------------------------------------------------------------------------------
+# Sample  DB  gene  allele  coverage  depth diffs uncertainty divergence  length  maxMAF  clusterid seqid annotation
+# PNUSAS002249  ARGannot.r1 Aac6-Iaa_AGly Aac6-Iy_761 100.0 26.435  14snp   3.196 438 0.067 454 761 no;yes;Aac6-Iy;AGly;AF144880;3542-3979;438
+# PNUSAS002249  ARGannot.r1 TEM-1D_Bla  TEM-198_1035  100.0 73.332  1snp    0.116 861 0.108 205 1035  no;no;TEM-198;Bla;AB700703;162-1022;861
+# ------------------------------------------------------------------------------
+
+# summary results file: srst2/PNUSAS002249-amr__genes__ARGannot.r1__results.txt
+# -----------------------------------------------------------------------------
+# Sample  Aac6-Iaa_AGly TEM-1D_Bla
+# PNUSAS002249  Aac6-Iy_761*  TEM-198_1035*
+# -----------------------------------------------------------------------------
+
+# You should be able to concatenate multiple full results files from multiple runs together.
+
+
+################################################################################
+## Run SRST2 for antibiotic resistance for multiple samples
+################################################################################
+
+## Get some help
+~/gdip/scripts/multisrst2.sh -h
+
+# Running this command will echo the following commands to STDOUT.
+~/gdip/scripts/multisrst2.sh
+# srst2 --log --input_pe PNUSAS002243_1.fastq.gz PNUSAS002243_2.fastq.gz --output srst2/amr_PNUSAS002243 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002244_1.fastq.gz PNUSAS002244_2.fastq.gz --output srst2/amr_PNUSAS002244 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002245_1.fastq.gz PNUSAS002245_2.fastq.gz --output srst2/amr_PNUSAS002245 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002246_1.fastq.gz PNUSAS002246_2.fastq.gz --output srst2/amr_PNUSAS002246 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002247_1.fastq.gz PNUSAS002247_2.fastq.gz --output srst2/amr_PNUSAS002247 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002248_1.fastq.gz PNUSAS002248_2.fastq.gz --output srst2/amr_PNUSAS002248 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002249_1.fastq.gz PNUSAS002249_2.fastq.gz --output srst2/amr_PNUSAS002249 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002250_1.fastq.gz PNUSAS002250_2.fastq.gz --output srst2/amr_PNUSAS002250 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002251_1.fastq.gz PNUSAS002251_2.fastq.gz --output srst2/amr_PNUSAS002251 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002252_1.fastq.gz PNUSAS002252_2.fastq.gz --output srst2/amr_PNUSAS002252 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002484_1.fastq.gz PNUSAS002484_2.fastq.gz --output srst2/amr_PNUSAS002484 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002485_1.fastq.gz PNUSAS002485_2.fastq.gz --output srst2/amr_PNUSAS002485 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+# srst2 --log --input_pe PNUSAS002486_1.fastq.gz PNUSAS002486_2.fastq.gz --output srst2/amr_PNUSAS002486 --gene_db /home/ubuntu/srst2/data/ARGannot.r1.fasta --threads 2
+
+## Pipe to bash to run it (in a GNU screen session)
+~/gdip/scripts/multisrst2.sh | bash
+
+## Or write a script
+# ~/gdip/scripts/multisrst2.sh > runsrst2.sh
+# cat runsrst2.sh
+# bash runsrst2.sh
+
+
+
+
+################################################################################
+## SRST2 for MLST for a single sample
+################################################################################
+
+cd ~/exampledata
+
 time srst2 --log \
   --input_pe PNUSAS002249_1.fastq.gz PNUSAS002249_2.fastq.gz \
   --output srst2/PNUSAS002249-mlst \
@@ -132,10 +195,10 @@ time srst2 --log \
   --mlst_definitions /opt/genomes/mlst_senterica/senterica.txt \
   --mlst_delimiter '-'
 
-# AMR search
-time srst2 --log \
-  --input_pe PNUSAS002249_1.fastq.gz PNUSAS002249_2.fastq.gz \
-  --output   srst2/PNUSAS002249-amr \
-  --gene_db  /home/ubuntu/srst2/data/ARGannot.r1.fasta
-
+# Takes about 15 minutes
+# Results file: srst2/PNUSAS002249-mlst__mlst__Salmonella_enterica__results.txt
+# -----------------------------------------------------------------------------
+# Sample  ST      aroC    dnaN    hemD    hisD    purE    sucA    thrA    mismatches      uncertainty     depth   maxMAF
+# PNUSAS002249    11?     5       2?      3       7       6       6       11      0       dnaN-2/edge2.0  20.4158571429   0.333333333333
+# -----------------------------------------------------------------------------
 
